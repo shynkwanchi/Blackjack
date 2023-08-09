aba4f66fd84dba931626e489f6028babb625756d
Nguyễn Quang Duy <duynguyenquang412@gmail.com>
2023-08-09 10:16:50 +0700

    Initial Commit

---
+++ Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Duy Nguyen Quang on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
